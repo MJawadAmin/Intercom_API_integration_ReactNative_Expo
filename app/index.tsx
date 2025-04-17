ed06ea4330fdc8ba395334c5f40049fedb9cdcc2
Jaddikhan <114812094+Jaddikhan@users.noreply.github.com>
2025-04-18 01:53:58 +0500

    addition

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,35 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Home screen</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#d2e1f3',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#1c0202',
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    margin: 10,
+    padding: 10,
+    borderWidth: 2,
+    borderColor: '#1c0202',
+    borderRadius: 10,
+    backgroundColor: '#f3f3f3',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 2,
+      height: 2,
+    },
+  }, // ✅ no extra parenthesis
+});
